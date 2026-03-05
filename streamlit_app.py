aad4192f8a6a47caab5cb5ad4180d3f60428dd61
user342859 <ken.altrex@gmail.com>
2026-03-05 15:14:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,20 @@ import streamlit.components.v1 as components
 target_url = "https://academic-genealogy.academyrh.info/"
 
 # This executes inside a protected iframe but can still trigger a top-level redirect
-components.html(
-    f"""
-    <script>
-        window.parent.location.href = "{target_url}";
-    </script>
-    """,
-    height=0,
-)
+#components.html(
+#    f"""
+#    <script>
+#        window.parent.location.href = "{target_url}";
+#    </script>
+#    """,
+#    height=0,
+#)
 
 # Using a meta tag for automatic redirection
-#st.markdown(
-#    f'<meta http-equiv="refresh" content="0; url={target_url}">',
-#    unsafe_allow_html=True
-#)
+st.markdown(
+    f'<meta http-equiv="refresh" content="0; url={target_url}">',
+    unsafe_allow_html=True
+)
 
 # Optional: Add a fallback link in case JavaScript is disabled
 st.write(f"Перенаправление на {target_url}...")
