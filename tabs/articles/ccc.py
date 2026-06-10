9ecc836dc3610ea963e9188b59ab49999c21a2c3
andr68373 <andr68373@gmail.com>
2026-06-10 10:46:59 +0300

    Update ccc.py

--- tabs/articles/ccc.py
+++ tabs/articles/ccc.py
@@ -3,3 +3,8 @@ import ccc
 
 print(ccc.articles)          # если в test.py есть some_variable = 42
 ccc.some_function()              # если в test.py есть def some_function(): ...
+
+message = "Привет из test.py!"
+
+def get_data():
+    return [1, 2, 3]
