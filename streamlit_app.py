6e57faa4d6bbf9f908dbd77b8fd3eaabdb2df253
andr68373 <andr68373@gmail.com>
2026-06-15 13:34:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,12 +92,6 @@ if _should_render_tab("school_search"):
             idx=ctx.idx,
         )
 
-if _ccc_render_tab("ccc_search"):
-    with tab_by_id["ccc_search"]:
-        render_ccc_search_tab(
-            df=ctx.df,
-            idx=ctx.idx,
-        )
 
 if _should_render_tab("intersection"):
     with tab_by_id["intersection"]:
