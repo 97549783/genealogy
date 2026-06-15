dd41111eff80d6bb5f0ad3143860ddfeebf0f26c
andr68373 <andr68373@gmail.com>
2026-06-15 15:02:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ from core.app import (
     render_app_header,
 )
 from tabs.articles.tab import render_articles_analysis_tab
-from tabs.file.articles import render_ccc_tab
 from tabs.intersection.tab import render_opponents_intersection_tab
 from tabs.lineages.tab import render_school_trees_tab
 from tabs.profiles.tab import render_profiles_tab
