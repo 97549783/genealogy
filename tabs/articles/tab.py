c1ff6033fa1e7a9c10591ec7b682ced5e333de5a
andr68373 <andr68373@gmail.com>
2026-06-15 14:30:58 +0300

    Update tab.py

--- tabs/articles/tab.py
+++ tabs/articles/tab.py
@@ -26,6 +26,7 @@ from .similar_schools_mode import render_similar_schools_mode
 from .semantic_imrad_mode import render_semantic_imrad_search_mode
 from .data import ALL_JOURNALS_KEY, filter_articles_by_journals, normalize_journal_key
 from .query_params import query_params_signature, should_hydrate_query
+from articles import ccc
 
 ARTICLE_MODE_LABELS = {
     "single_school": "Анализ одной школы",
