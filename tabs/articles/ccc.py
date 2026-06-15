9770dd097fdc0e54fb28a4da59010c68e397b1bb
andr68373 <andr68373@gmail.com>
2026-06-15 11:16:29 +0300

    Update ccc.py

--- tabs/articles/ccc.py
+++ tabs/articles/ccc.py
@@ -1,5 +1,7 @@
-file_path = "articles/ccc.py"
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent
+file_path = base_dir / "articles" / "ccc.py"
 
 with open(file_path, "r", encoding="utf-8") as f:
-    content = f.read()
-    print(content)
+    print(f.read())
