fe75182b0ecb0d16e4afabfcc80b61790a4f35c7
andr68373 <andr68373@gmail.com>
2026-06-15 22:54:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -228,8 +228,8 @@ if _should_render_tab("articles_comparison"):
     with tab_by_id["articles_comparison"]:
         render_articles_analysis_tab(df_lineage=ctx.df, idx_lineage=ctx.idx)
 
-if _should_render_tab("test1111"):
-    with tab_by_id["test1111"]:
-        test111()
+if _should_render_tab("test111"):
+    with tab_by_id["test111"]:
+        test1111()
 
 
