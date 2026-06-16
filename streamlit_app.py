faa239efb5eee9e467f3a374c14e5436d68f993c
andr68373 <andr68373@gmail.com>
2026-06-16 10:40:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ from tabs.registry import DEFAULT_TAB_ID, TAB_ID_TO_LABEL, TAB_SPECS
 from tabs.school_analysis.tab import render_school_analysis_tab
 from tabs.school_comparison.tab import render_school_comparison_tab
 from tabs.school_search.tab import render_school_search_tab
-from tabs.test1111 import test1
+from tabs.test1111 import test1111
 
 
 MAIN_TAB_STATE_KEY = "main_tab"
@@ -117,5 +117,5 @@ if _should_render_tab("articles_comparison"):
 
 if _should_render_tab("test1111"):
     with tab_by_id["test1111"]:
-        render_test1()
+        render_test1111_tab()
 
