333072b3cbe988b3ed3d32996305c3e258f87778
andr68373 <andr68373@gmail.com>
2026-06-17 17:46:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,8 +112,6 @@ if _should_render_tab("school_comparison"):
             idx=ctx.idx,
         )
 
-if _should_render_tab("test1111"):
-    with tab_by_id["test1111"]:
-    test1111.test1111(test1111)
+
         
   
